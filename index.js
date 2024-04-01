459342d43c285d595cb7f0efe569faa359fdbcd1
omolo-j <omoloj135@gmail.com>
2024-04-01 16:27:30 +0300

    event listening lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', function() {
+        alert('I was clicked!');
+    });
 }
+
+addingEventListener(); // Call the function to bind the event listener
+
+function clickAlert() {
+    alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
